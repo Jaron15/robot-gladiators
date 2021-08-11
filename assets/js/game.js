30c51e0fbb49b2d20f0c36bdf493f2c2e44bdf04
Jaron <jjkenyon777@gmail.com>
2021-08-10 18:40:45 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,7 @@ while(playerHealth > 0 && enemyHealth > 0) {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             //subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }
@@ -33,7 +33,8 @@ while(playerHealth > 0 && enemyHealth > 0) {
     }
 
     //subtract the value of 'player attack' from the value of 'enemyHealth' and use that result to update the value in the `enemyHealth` variable
-    enemyHealth = enemyHealth - playerAttack;
+   var damage = randomNumber(playerAttack - 3, playerAttack)
+    enemyHealth = Math.max(0, enemyHealth - damage);
     // Log a resulting message to the console so we know that it worked.
 console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -51,7 +52,8 @@ console.log(
     }
 
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -82,7 +84,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 
     var pickedEnemyName = enemyNames[i];
 
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     
     fight(pickedEnemyName);
 
@@ -159,4 +161,10 @@ var shop = function() {
             break;
     }
 };
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
+
 startGame();
